d803f84f4c60a26060d05c86657d7c046fe98c2a
KrivovaA <onetwothreesfu@gmail.com>
2019-07-15 19:36:37 +0700

    Updates

---
+++ precache-manifest.742dada6a46230235493934171a83c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767cb3609801fe68a41d2431040c63c9",
+    "url": "/test-colored-userpic/index.html"
+  },
+  {
+    "revision": "9d614496e9357b7f19d4",
+    "url": "/test-colored-userpic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/test-colored-userpic/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "9d614496e9357b7f19d4",
+    "url": "/test-colored-userpic/static/js/main.a866cf6b.chunk.js"
+  },
+  {
+    "revision": "2e84edab63fda7ff402b",
+    "url": "/test-colored-userpic/static/js/runtime~main.0a607665.js"
+  },
+  {
+    "revision": "ae9774417e612b45401ae59a462013d8",
+    "url": "/test-colored-userpic/static/media/pic.ae977441.jpg"
+  }
+]);
